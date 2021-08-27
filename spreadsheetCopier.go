b8c4304f894998e9c30c83ed45c34c8e612bde63
Grace Thompson <grace.thompson97@gmail.com>
2021-08-27 15:57:31 -0700

    Allow user to enter file and sheet name

---
+++ spreadsheetCopier.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/xuri/excelize/v2"
+	"log"
+	"strconv"
+    "os"
+    "strings"
+)
+
+func main() {
+	fmt.Print("Enter the name of the file to be copied: ")
+	reader := bufio.NewReader(os.Stdin)
+	fileInput, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print("Enter the name of the Sheet you would like to copy: ")
+	newReader := bufio.NewReader(os.Stdin)
+	sheetInput, err := newReader.ReadString('\n')
+	fileInput = strings.TrimSpace(fileInput)
+	sheetInput = strings.TrimSpace(sheetInput)
+
+	originalFile, err := excelize.OpenFile(fileInput)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	newFile := excelize.NewFile()
+	rows, err := originalFile.GetRows(sheetInput)
+	i := 0
+	for _, row := range rows {
+		for _, colCell := range row {
+			i++
+			cell := strconv.Itoa(i)
+			cell = "A" + cell
+			newFile.SetCellValue("Sheet1", cell, colCell)
+
+		}
+	}
+	if err := newFile.SaveAs("Book2.xlsx"); err != nil {
+		fmt.Println(err)
+	}
+}
