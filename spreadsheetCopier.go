0fb2972a8977203d99830f3b438056c596a3b1c2
Grace Thompson <grace.thompson97@gmail.com>
2021-08-28 10:17:48 -0700

    add row selection

--- spreadsheetCopier.go
+++ spreadsheetCopier.go
@@ -12,17 +12,29 @@ import (
 
 func main() {
 	fmt.Print("Enter the name of the file to be copied: ")
-	reader := bufio.NewReader(os.Stdin)
-	fileInput, err := reader.ReadString('\n')
+	fileReader := bufio.NewReader(os.Stdin)
+	fileInput, err := fileReader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Print("Enter the name of the Sheet you would like to copy: ")
-	newReader := bufio.NewReader(os.Stdin)
-	sheetInput, err := newReader.ReadString('\n')
+	sheetReader := bufio.NewReader(os.Stdin)
+	sheetInput, err := sheetReader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print("Starting from which row would you like to copy? ")
+	rowReader := bufio.NewReader(os.Stdin)
+	rowInput, err := rowReader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fileInput = strings.TrimSpace(fileInput)
 	sheetInput = strings.TrimSpace(sheetInput)
-
+	rowInput = strings.TrimSpace(rowInput)
+	rowNum, err := strconv.Atoi(rowInput)
+	rowNum--
 	originalFile, err := excelize.OpenFile(fileInput)
 	if err != nil {
 		fmt.Println(err)
@@ -30,11 +42,10 @@ func main() {
 	}
 	newFile := excelize.NewFile()
 	rows, err := originalFile.GetRows(sheetInput)
-	i := 0
 	for _, row := range rows {
 		for _, colCell := range row {
-			i++
-			cell := strconv.Itoa(i)
+			rowNum++
+			cell := strconv.Itoa(rowNum)
 			cell = "A" + cell
 			newFile.SetCellValue("Sheet1", cell, colCell)
 
